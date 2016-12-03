85dae70e386a90ec97f9e2bcd18795ba7bd0b771
ian shirley <ian shirley>
2016-12-02 18:48:19 -0600

    changed ajax to https

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -7,7 +7,7 @@ function displayGifInfo() {
 	var topic = $(this).attr("data-name");
 
 	// This is the API url and key
-	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=dc6zaTOxFJmzC&limit=10";
+	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=dc6zaTOxFJmzC&limit=10";
 
     // Performing an AJAX request with the queryURL
     $.ajax({ url: queryURL, method: "GET"}).done(function(response) {
